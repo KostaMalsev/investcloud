f612a8e9c6c5805394da65cc3d8eb8a7a2bcd8de
Kosta Malsev <51965607+KostaMalsev@users.noreply.github.com>
2020-09-20 15:12:34 +0300

    Update server.js

--- server.js
+++ server.js
@@ -17,7 +17,7 @@ server.use((req, res, next) => {
     getRequest('https://investcloud.herokuapp.com/db', (data)) => {
       putRequest(data, 'https://jsonblob.com/api/jsonBlob/25727a48-fb31-11ea-9b5c-1dd302ffc285', () => { console.log('Created backup.') })
     })
-    */
+    
   req.body.createdAt = Date.now()
   }
   // Continue to JSON Server router
