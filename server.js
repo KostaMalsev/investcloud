009a1fe18e68fa70c956a75f999fec65dffe4989
BHMGaming <34835685+BHMGaming@users.noreply.github.com>
2020-09-20 18:00:54 +0300

    adding remote storage

--- server.js
+++ server.js
@@ -29,17 +29,16 @@ server.use((req, res, next) => {
     next()
 });
 
-// Use default router
-server.listen(port, () => {
-    getRequest('https://jsonblob.com/api/jsonBlob/25727a48-fb31-11ea-9b5c-1dd302ffc285', (data) => {
-        putRequest(data, 'https://investcloud.herokuapp.com/db',
-            () => {
-                console.log('Restored from backup.')
-            });
-    });
+getRequest('https://jsonblob.com/api/jsonBlob/25727a48-fb31-11ea-9b5c-1dd302ffc285', (data) => {
+    putRequest(data, 'https://investcloud.herokuapp.com/db',
+        () => {
+            console.log('Restored from backup.')
+        });
 });
 
 
+// Use default router
+server.listen(port);
 
 // HTTP Request
     function getRequest(url, callback) {
