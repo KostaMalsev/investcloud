971d0d8df780bccbb57bec52e747dcf95b1d1d5c
Kosta Malsev <51965607+KostaMalsev@users.noreply.github.com>
2020-09-20 14:21:48 +0300

    Update server.js

--- server.js
+++ server.js
@@ -1,3 +1,64 @@
+const jsonServer = require('json-server')
+const server = jsonServer.create()
+const router = jsonServer.router('db.json')
+const middlewares = jsonServer.defaults()
+
+// Set default middlewares (logger, static, cors and no-cache)
+server.use(middlewares)
+
+// Add custom routes before JSON Server router
+server.get('/echo', (req, res) => {
+  res.jsonp(req.query)
+})
+
+// To handle POST, PUT and PATCH you need to use a body-parser
+// You can use the one used by JSON Server
+server.use(jsonServer.bodyParser)
+server.use((req, res, next) => {
+  if (req.method === 'POST' || req.method === 'PUT') {
+    getRequest('https://investcloud.herokuapp.com/db', (data) => {
+      putRequest(data, 'https://jsonblob.com/api/jsonBlob/25727a48-fb31-11ea-9b5c-1dd302ffc285', () => { console.log('Created backup.') })
+    })
+  //req.body.createdAt = Date.now()
+  }
+  // Continue to JSON Server router
+  next()
+})
+
+// Use default router
+server.use(router)
+server.listen((process.env.PORT || 3000), () => {
+  getRequest('https://jsonblob.com/api/jsonBlob/25727a48-fb31-11ea-9b5c-1dd302ffc285', (data) => {
+    putRequest(data, 'https://investcloud.herokuapp.com/db', () => { console.log('Restored from backup.') })
+  }
+})
+
+
+// HTTP Request
+function getRequest(url, callback) {
+    var xmlhttp = new XMLHttpRequest();
+    xmlhttp.onreadystatechange = function() {
+        if (this.readyState == 4 && this.status == 200) {
+            callback(JSON.parse(this.responseText));
+        }
+    }
+    xmlhttp.open('GET', url, true); 
+    xmlhttp.send();
+}
+
+function putRequest(data, url, callback) {
+    var xmlhttp = new XMLHttpRequest();
+    xmlhttp.onreadystatechange = function() {
+        if (this.readyState == 4 && this.status == 200) {
+            callback(this.responseText);
+        }
+    }
+    xmlhttp.open('PUT', url, true);
+    xmlhttp.setRequestHeader('Content-Type', 'application/json; charset=UTF-8');
+    xmlhttp.send(JSON.stringify(data));
+}
+
+/*
 const jsonServer = require('json-server');
 const server = jsonServer.create();
 const router = jsonServer.router('db.json');
@@ -8,3 +69,4 @@ server.use(middlewares);
 server.use(router);
 
 server.listen(port);
+*/
