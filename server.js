4aedf21a75f3cf6cb48c70fb956c3481ee050e35
Kosta Malsev <51965607+KostaMalsev@users.noreply.github.com>
2020-09-20 14:51:38 +0300

    Update server.js

--- server.js
+++ server.js
@@ -11,8 +11,9 @@ server.use(router);
 // You can use the one used by JSON Server
 server.use(jsonServer.bodyParser)
 server.use((req, res, next) => {
+  //Change to enabgle remote storage CHANGE_200920
   if (req.method === 'POST' || req.method === 'PUT') {
-    getRequest('https://investcloud.herokuapp.com/db', (data) => {
+    getRequest('https://investcloud.herokuapp.com/db', (data)) => {
       putRequest(data, 'https://jsonblob.com/api/jsonBlob/25727a48-fb31-11ea-9b5c-1dd302ffc285', () => { console.log('Created backup.') })
     })
   //req.body.createdAt = Date.now()
