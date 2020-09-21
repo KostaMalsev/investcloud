9828b9ad608cb0880161a701f8040d5fc168e2b1
BHMGaming <34835685+BHMGaming@users.noreply.github.com>
2020-09-21 11:47:56 +0300

    adding remote storage

--- server.js
+++ server.js
@@ -15,7 +15,8 @@ server.use(router);
 // To handle POST, PUT and PATCH you need to use a body-parser
 // You can use the one used by JSON Server
 server.use(jsonServer.bodyParser)
-server.use((req, res, next) => {
+//server.use((req, res, next) => {
+server.post('/post/user', (req, res) => {
     //Change to enable remote storage CHANGE_200920
     console.log(`${req.method}`);
     if (req.method === 'POST') {
