1b512af675ad39df6541821016a36e4c99ee4081
BHMGaming <34835685+BHMGaming@users.noreply.github.com>
2020-09-22 20:09:42 +0300

    adding remote storage

--- server.js
+++ server.js
@@ -5,51 +5,88 @@ const middlewares = jsonServer.defaults();
 const port = process.env.PORT || 3000;
 var XMLHttpRequest = require('xhr2');
 
-var AfterRestore=false;
+var Restoring=false;
 
 
 server.use(jsonServer.bodyParser)
+
+//Using middlewares with express:
 server.use(middlewares);
 
-/*
-server.use((req, res,next) => {
 
+//Set the working urls:
+const usr_backup_url = 'https://jsonblob.com/api/jsonBlob/25727a48-fb31-11ea-9b5c-1dd302ffc285';
+const sim_param_backup_url = 'https://jsonblob.com/api/893223c1-fc27-11ea-a8f0-8decf7d8c81c';
+const usr_data_url = 'https://investcloud.herokuapp.com/posts';
+const sim_param_url = 'https://investcloud.herokuapp.com/comments';
+
+
+//Catch POST events:
+server.use( async (req, res,next) => {
     //Change to enable remote storage CHANGE_200920
     console.log('Creating backup...');
-    let post_url_usr = 'https://investcloud.herokuapp.com/posts';
-    if ((req.method === 'POST') && (req.baseUrl=== post_url_usr) && (AfterRestore)) {
-        console.log('Creating backup inside if...');
-        getRequest(post_url_usr, (data) => {
-            putRequest(data, 'https://jsonblob.com/api/jsonBlob/25727a48-fb31-11ea-9b5c-1dd302ffc285', () => {
-                    console.log('Created usr answers backup.');
-            });
-        });
-    }else{
-        let comments_url = 'https://jsonblob.com/api/893223c1-fc27-11ea-a8f0-8decf7d8c81c';
-        if(req.baseUrl === comments_url)
-        {
-            putRequest(data, 'https://jsonblob.com/api/jsonBlob/25727a48-fb31-11ea-9b5c-1dd302ffc285', () => {
-                console.log('Created sim param backup.');});
+    //Dont perform archive if resotring:
+    if(Restoring) next();
+
+    //Making the backup data for user answers and sim data:
+    if (req.method === 'POST') {
+            if (req.req.baseUrl == usr_data_url) {
+                //Get the data from in memory db:
+                var data = await httpRequest('GET', usr_data_url, '');
+                //Store backup at remote db:
+                var responce = await httpRequest('PUT', usr_backup_url, data);
+            }
+            //If it's simulation params create a backup :
+            if (req.req.baseUrl == sim_param_url) {
+                //Get the data from in memory db:
+                var data = await httpRequest('GET', sim_param_url, '');
+                //Store backup at remote db:
+                var responce = await httpRequest('PUT', sim_param_backup_url, data);
+            }
         }
-    }
-    // Continue to JSON Server router
-    next()
+    next();
 });
- */
 
 
-server.use(router);
+
 // Use default router
-server.listen(port, () => {
+server.use(router);
+
+//Start listen to port:
+server.listen(port, async () => {
+
+    //Perform Restore on start:
     console.log('Started restoration...');
-    /*
+    Restoring = true;//mark the restorations started
+    //Fetch backup from remote archive:
+    var data = await httpRequest('GET', usr_backup_url);
+    //Push rows of data to app db:
+    data.forEach(async function (row, index) {
+        var res = await httpRequest('POST', usr_data_url, row);
+    });
+    //Fetch comments from remote archive:
+    var comments_data = await httpRequest('GET', sim_param_backup_url);
+    //Push it to the in-memory db:
+    var res = await httpRequest('POST', sim_param_url, comments_data);
+    Restoring = false;//finished restoring
+    console.log('Finished restoration...');
+})
+
+
+
+
+
+
+
+
+
+/*
     //let data = await getRequest(url, (d) => {return d});
     getRequest('https://jsonblob.com/api/jsonBlob/25727a48-fb31-11ea-9b5c-1dd302ffc285', (data) => {
         data.forEach(function(row, index) {
             postRequest(row, 'https://investcloud.herokuapp.com/posts', () => {
                 //console.log('Posting user num. '+index.toString());
                 console.log("restoring row usr");
-
                 let comments_url = 'https://jsonblob.com/api/893223c1-fc27-11ea-a8f0-8decf7d8c81c';
                 //Restore the simulation settings:
                 getRequest(comments_url, (data) => {
@@ -65,8 +102,39 @@ server.listen(port, () => {
 
         });
     });
-     */
 });
+*/
+
+
+
+//Async function to retrieve and get data: GET,PUT,POST
+function httpRequest(type, url, data) {
+    return new Promise(resolve => {
+        var xmlhttp = new XMLHttpRequest();
+        if( (type=='POST' || type=='PUT' || type=='GET')) {
+            xmlhttp.onreadystatechange = function () {
+                let status_ = (type == 'POST' || type == 'PUT') ? 201 : 200;
+                if (this.readyState == 4 && this.status == status_) {
+                    resolve(JSON.parse(this.responseText));
+                }
+            };
+            xmlhttp.open(type, url, true);
+            xmlhttp.setRequestHeader('Content-Type', 'application/json; charset=UTF-8');
+            xmlhttp.send(type == 'POST' || type == 'PUT' ? JSON.stringify(data) : null);
+        }
+    });
+}
+
+
+
+
+
+
+
+
+
+
+
 
 
 // HTTP Request
@@ -85,7 +153,7 @@ function getRequest(url, callback) {
 function putRequest(data, url, callback) {
     var xmlhttp = new XMLHttpRequest();
     xmlhttp.onreadystatechange = function () {
-        if (this.readyState == 4 && this.status == 200) {
+        if (this.readyState == 4 && this.status == 201) {
             callback(this.responseText);
         }
     };
@@ -97,7 +165,7 @@ function putRequest(data, url, callback) {
 function postRequest(data, url, callback) {
     var xmlhttp = new XMLHttpRequest();
     xmlhttp.onreadystatechange = function () {
-        if (this.readyState == 4 && this.status == 200) {
+        if (this.readyState == 4 && this.status == 201) {
             callback(this.responseText);
         }
     };
@@ -123,20 +191,32 @@ async function a() {
     //           | Without an await parameter, httpRequest will run in the background. The rest of the code will continue running
     console.log(post); // Therefore "post" will be undefined.
 }
-/*
-
-function httpRequest(type, url, data) {
-    return new Promise(resolve => {
-        var xmlhttp = new XMLHttpRequest();
-        xmlhttp.onreadystatechange = function () {
-            if (this.readyState == 4 && this.status == 200) {
-                resolve(JSON.parse(this.responseText));
-            }
-        };
-        xmlhttp.open(type, url, true);
-        xmlhttp.setRequestHeader('Content-Type', 'application/json; charset=UTF-8');
-        xmlhttp.send(type == 'POST' || type == 'PUT' ? JSON.stringify(data) : null);
-    }
-}
 */
 
+
+
+
+
+
+//--------------------------
+/*
+  let post_url_usr = 'https://investcloud.herokuapp.com/posts';
+  if ((req.method === 'POST') && (req.baseUrl=== post_url_usr) && (AfterRestore)) {
+      console.log('Creating backup inside if...');
+      getRequest(post_url_usr, (data) => {
+          putRequest(data, 'https://jsonblob.com/api/jsonBlob/25727a48-fb31-11ea-9b5c-1dd302ffc285', () => {
+                  console.log('Created usr answers backup.');
+          });
+      });
+  }else{
+      let comments_url = 'https://jsonblob.com/api/893223c1-fc27-11ea-a8f0-8decf7d8c81c';
+      if(req.baseUrl === comments_url)
+      {
+          putRequest(data, 'https://jsonblob.com/api/jsonBlob/25727a48-fb31-11ea-9b5c-1dd302ffc285', () => {
+              console.log('Created sim param backup.');});
+      }
+  }
+  // Continue to JSON Server router
+  next()
+});
+*/
